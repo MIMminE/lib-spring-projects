1091b189507b8f1816523932fcb70b3abafb11c5
MIMminE <weluv.ww@gmail.com>
2025-09-16 09:12:18 +0900

    feat: initialize Spring Boot Redis study project

---
+++ spring-data/redis/src/main/kotlin/lib_spring_data/study_redis/StudyRedisApplication.kt
@@ -0,0 +1,11 @@
+package lib_spring_data.study_redis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyRedisApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudyRedisApplication>(*args)
+}
