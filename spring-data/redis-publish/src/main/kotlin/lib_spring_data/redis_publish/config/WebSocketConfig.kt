9e463ac2e0ad9c1e9d3a3a8fd719b9304e596148
MIMminE <weluv.ww@gmail.com>
2025-09-18 16:46:34 +0900

    feat: 웹 소켓 기반의 간단한 채팅 프로그램 개발 및 테스트 완료
    - 레디스를 활용하는 방안으로 채팅 프로그램 업그레이드 예정
    - 스프링 레디스 API 사용 방법과 레딧 pub/sub 이점 학습 목적

---
+++ spring-data/redis-publish/src/main/kotlin/lib_spring_data/redis_publish/config/WebSocketConfig.kt
@@ -0,0 +1,20 @@
+package lib_spring_data.redis_publish.config
+
+import lib_spring_data.redis_publish.socket.ChatWebSocketHandler
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+
+@Configuration
+@EnableWebSocket
+class WebSocketConfig(
+
+    private val chatWebSocketHandler: ChatWebSocketHandler
+) : WebSocketConfigurer {
+
+    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
+        registry
+            .addHandler(chatWebSocketHandler, "/chat")
+    }
+}
--- spring-data/redis/src/main/kotlin/lib_spring_data/study_redis/StudyRedisApplication.kt
+++ spring-data/redis/src/main/kotlin/lib_spring_data/study_redis/StudyRedisApplication.kt
@@ -10,3 +10,16 @@ class StudyRedisApplication()
 fun main(args: Array<String>) {
     runApplication<StudyRedisApplication>(*args)
 }
+
+
+데이터 크기별 조회 시간:
+
+Redis (Hash Table):
+- 1,000개: O(1) = 0.1ms
+- 1,000,000개: O(1) = 0.1ms  ← 크기와 무관!
+- 1,000,000,000개: O(1) = 0.1ms
+
+MySQL (B+ Tree + 디스크):
+- 1,000개: O(log n) = 1ms + 10ms(디스크) = 11ms
+- 1,000,000개: O(log n) = 3ms + 10ms(디스크) = 13ms  
+- 1,000,000,000개: O(log n) = 5ms + 10ms(디스크) = 15ms
